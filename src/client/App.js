40484d873659b143419cd7be2efd77f04c1dda4f
Almaha <mahaw999@gmail.com>
2023-06-04 11:33:10 +0300

    adding sass files

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Item from "./Item";
+import '../styles/header.scss'
+import '../styles/input-sec.scss'
 
 export default function App() {
     let [items, setItems] = useState([])
@@ -22,15 +24,17 @@ export default function App() {
     <h1>SHOPPING LIST</h1>
     <section id="list">
     <h2>Items to buy:</h2>
+    <section id="show-inputs">
     <form onSubmit={onSubmit}>
         <input type="text" name="item" placeholder="add an item to the list" required />
         <button>add</button>
     </form>
-    <ul>
+    <ul id="items">
     {items.map((item, index) => (
         <Item   key={item+index}  item={item} onRemove={onRemove} />
     ))}
     </ul>
+    </section>
     </section>
     </>
  )
