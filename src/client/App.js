e0ca6f68fd4effe6eea0eee88fbbcd94fb0cc1e6
Almaha <mahaw999@gmail.com>
2023-06-11 20:55:24 +0300

    add local storage & add comments

--- src/client/App.js
+++ src/client/App.js
@@ -7,19 +7,21 @@ import { faMoon } from "@fortawesome/free-solid-svg-icons";
 import { faSun } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
-    const [input, setInput] = useState("");
-    let [items, setItems] = useState([]); //handle form data and any update by user; store it in array
+//local storage variables
+    let itemsStorage = JSON.parse(localStorage.getItem('items')); 
+    let itemsNOStorage = JSON.parse(localStorage.getItem('countItems')); 
+//usestate
+    let [items, setItems] = useState(itemsStorage); //handle form data and any update by user; store it in array
     let [darkMode, setDarkMode] = useState(false); //to make dark theme
     let [lightMode, setLightMode] = useState(true); //to make light theme 
-    let [countItem, setCountItem] = useState(0); //to count completed tasks
+    let [countItem, setCountItem] = useState(itemsNOStorage); //to count completed tasks
 
-    //remove one item at a time
+//remove one item at a time
     let onRemove = (itemRemove) => {
         setItems(items.filter((i) => i !== itemRemove));
         setCountItem(countItem - 1);
     }
-
-    //handle form inputs and store it in state(array)
+//handle form inputs and store it in state(array)
     let onSubmit = (e) => {
         e.preventDefault();
         const form = e.target;
@@ -28,11 +30,19 @@ export default function App() {
         setItems(newItem);
         form.reset();
     }
-    //count number of items
+//keep track of items and store it in localStorage
+    useEffect(()=>{
+        localStorage.setItem('items', JSON.stringify(items))
+    }, [items])
+//return number of items
     let onCount = () => {
       return countItem
     }
-    //apply dark/light mode
+//keep track of number of items and store it in localStorage
+    useEffect(()=>{
+        localStorage.setItem('countItems', JSON.stringify(countItem))
+    }, [countItem])
+//apply dark/light mode
     useEffect(() => {
         if(darkMode){
             document.body.classList.add('darkMood')
@@ -42,7 +52,7 @@ export default function App() {
     useEffect(() => {
         if(darkMode){
             document.body.classList.remove('darkMood')
-        } 
+        }
     }, [lightMode])
 
  return(
@@ -58,7 +68,7 @@ export default function App() {
     <h2> {onCount()} Items to buy:</h2>
     <section id="show-inputs">
     <form onSubmit={onSubmit}>
-        <input type="text" value={input} onInput={(e)=>setInput(e.target.value)} name="item" placeholder="add an item to the list" autoComplete="no" required />
+        <input type="text" name="item" placeholder="add an item to the list" autoComplete="no" required />
         <button id="add-btn" onClick={()=> setCountItem(countItem + 1)}>add</button>
     </form>
     <ul id="items">
